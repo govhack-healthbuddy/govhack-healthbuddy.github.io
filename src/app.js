e03ed449c17277a8d7911de150a52257b223a3e4
Russell Lescai <russ.lescai@hotmail.com>
2015-07-04 17:07:31 +0930

    ...

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when("/home", { templateUrl: "main.html", controller: "mainController" }).
       when("/demo", { templateUrl: "demo.html", controller: "demoController" }).
+      when("/demo2", { templateUrl: "demo2.html", controller: "demoController" }).
+      when("/demo3", { templateUrl: "demo3.html", controller: "demoController" }).
       when("/results/:facilityTypeCode/:latitude/:longitude/:gender/:isChild", { templateUrl: "results.html", controller: "resultsController" }).
       when("/taxi/", { templateUrl: "taxi.html", controller: "taxiController" }).
       otherwise({ redirectTo: '/home' });
